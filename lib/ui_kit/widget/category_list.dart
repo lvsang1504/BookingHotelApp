25f181856221e0e4d4c391879318e7e448c49c36
admin <luongsang010s@gmail.com>
2022-06-18 23:32:24 +0700

    config window

--- lib/ui_kit/widget/category_list.dart
+++ lib/ui_kit/widget/category_list.dart
@@ -2,8 +2,11 @@ import 'package:bloc/bloc.dart';
 import 'package:book_hotel/module/home/home_bloc.dart';
 import 'package:book_hotel/module/home/ui/room_by_category.dart';
 import 'package:book_hotel/shared_code/model/category_model.dart';
+import 'package:book_hotel/shared_code/responsive/responsive.dart';
+import 'package:book_hotel/utils/materials/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:vertical_tabs_flutter/vertical_tabs.dart';
 
 class CategoryList extends StatefulWidget {
   final List<CategoryModel> categories;
@@ -18,6 +21,7 @@ class _CategoryListState extends State<CategoryList>
     with SingleTickerProviderStateMixin {
   late final List<CategoryModel> categories;
   late TabController _tabController;
+  int categoryID = 0;
 
   @override
   void initState() {
@@ -38,45 +42,142 @@ class _CategoryListState extends State<CategoryList>
       length: categories.length,
       child: Scaffold(
         backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(40.0),
-          child: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            bottom: TabBar(
-              controller: _tabController,
-              indicatorColor: Colors.grey,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 1.0,
-              unselectedLabelColor: Colors.grey,
-              labelColor: Colors.black,
-              isScrollable: true,
-              tabs: categories.map((CategoryModel categoryModel) {
-                return Container(
-                    padding: const EdgeInsets.only(bottom: 8.0, top: 10.0),
-                    child: Text(
-                      categoryModel.name!,
-                      style: const TextStyle(
-                          color: Colors.black38,
-                          fontFamily: "Gotik",
-                          fontWeight: FontWeight.w800,
-                          fontSize: 14.0)),
+        appBar: !Responsive.isDesktop(context)
+            ? PreferredSize(
+                preferredSize: const Size.fromHeight(40.0),
+                child: AppBar(
+                  elevation: 0,
+                  backgroundColor: Colors.transparent,
+                  bottom: TabBar(
+                    controller: _tabController,
+                    indicatorColor: Colors.grey,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicatorWeight: 1.0,
+                    unselectedLabelColor: Colors.grey,
+                    labelColor: Colors.black,
+                    isScrollable: true,
+                    tabs: categories.map((CategoryModel categoryModel) {
+                      return Container(
+                        padding: const EdgeInsets.only(bottom: 8.0, top: 10.0),
+                        child: Text(categoryModel.name!,
+                            style: const TextStyle(
+                                color: Colors.black38,
+                                fontFamily: "Gotik",
+                                fontWeight: FontWeight.w800,
+                                fontSize: 14.0)),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              )
+            : const PreferredSize(
+                preferredSize: Size.fromHeight(40.0),
+                child: SizedBox(),
+              ),
+        body: !Responsive.isDesktop(context)
+            ? TabBarView(
+                controller: _tabController,
+                physics: const NeverScrollableScrollPhysics(),
+                children: categories.map((CategoryModel categoryModel) {
+                  return CategoryRooms(
+                    categoryId: categoryModel.id.toString(),
+                    homeBloc: widget.bloc,
+                  );
+                }).toList(),
+              )
+            : Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: VerticalTabs(
+                  tabsWidth: 400,
+                  tabs: categories.map((CategoryModel categoryModel) {
+                    return Tab(
+                      child: Container(
+                        padding: const EdgeInsets.only(bottom: 8.0, top: 10.0),
+                        child: Text(categoryModel.name!,
+                            style: const TextStyle(
+                                color: Colors.black38,
+                                fontFamily: "Gotik",
+                                fontWeight: FontWeight.w800,
+                                fontSize: 14.0)),
+                      ),
                     );
-              }).toList(),
+                  }).toList(),
+                  contents: categories.map((CategoryModel categoryModel) {
+                    return CategoryRooms(
+                      categoryId: categoryModel.id.toString(),
+                      homeBloc: widget.bloc,
+                    );
+                  }).toList(),
+                  backgroundColor: Colors.white,
+                  onSelect: (int tabIndex) {},
+                  indicatorSide: IndicatorSide.start,
+                ),
             ),
-          ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          physics: const NeverScrollableScrollPhysics(),
-          
-          children: categories.map((CategoryModel categoryModel) {
-            return CategoryRooms(
-              categoryId: categoryModel.id.toString(),
-              homeBloc: widget.bloc,
-            );
-          }).toList(),
-        ),
+        // : SingleChildScrollView(
+        //     child: SizedBox(
+        //       height: 500,
+        //       child: NavigationRail(
+        //         onDestinationSelected: (int index) {
+        //           setState(() {
+        //             categoryID = index;
+        //           });
+        //         },
+        //         destinations:
+        //             categories.map((CategoryModel categoryModel) {
+        //           return NavigationRailDestination(
+        //             icon: Column(
+        //               children: [
+        //                 const Icon(Icons.favorite_border),
+        //                 Text(categories[categoryID].name!)
+        //               ],
+        //             ),
+        //             selectedIcon: Container(
+        //               color: AppColor.colorGrey,
+        //               child: Column(
+        //                 children: [
+        //                   const Icon(Icons.favorite_border),
+        //                   Text(categories[categoryID].name!)
+        //                 ],
+        //               ),
+        //             ),
+        //             label: const Text(''),
+        //           );
+        //           // return CategoryRooms(
+        //           //   categoryId: categoryModel.id.toString(),
+        //           //   homeBloc: widget.bloc,
+        //           // );
+        //         }).toList(), selectedIndex: null,
+        //         // selectedIndex: categoryID,
+        //       ),
+        //     ),
+        //   )
+        // : Row(
+        //     children: [
+        //       SizedBox(
+        //         width: MediaQuery.of(context).size.width * 0.3,
+        //         child: ListView.builder(
+        //           itemCount: categories.length,
+        //           itemBuilder: (context, index) {
+        //             return GestureDetector(
+        //               onTap: () => setState(() {
+        //                 categoryID = index;
+        //               }),
+        //               child: Padding(
+        //                 padding: const EdgeInsets.all(8.0),
+        //                 child: Text(categories[index].name!),
+        //               ),
+        //             );
+        //           },
+        //         ),
+        //       ),
+        //       Expanded(
+        //         child: CategoryRooms(
+        //           categoryId: categories[categoryID].id.toString(),
+        //           homeBloc: widget.bloc,
+        //         ),
+        //       )
+        //     ],
+        //   ),
       ),
     );
   }
